fb23a9f3ebcfe72f767314e0e799a5761ecdd3fa
Roman_Chub <rchub@inbox.ru>
2022-05-17 01:20:48 +0300

    added a method to print the original array

---
+++ test/Program.cs
@@ -0,0 +1,9 @@
+﻿void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"'{array[i]}', ");
+    }
+    Console.WriteLine("'{0}']", array[array.Length - 1]);
+}
